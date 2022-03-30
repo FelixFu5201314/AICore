65b2b11cd1132d057ea9e163af18a44bf531a6f6
f4396 <fafu@lusterinc.com>
2022-03-30 17:59:37 +0800

    模型

---
+++ dao/models/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Author:FelixFu
+# @Date: 2021.4.14
+# @GitHub:https://github.com/felixfu520
+# @Copy From:
+
+# 1. BackBone
+from .backbone import TIMM
+
+# 2. ImageClassification
+from .ImageClassification import TIMMC
