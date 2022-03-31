6f356c8152b7d884178078428811527fa0f6cb76
f4396 <fafu@lusterinc.com>
2022-03-31 10:16:17 +0800

    交叉熵损失函数

---
+++ dao/losses/CrossEntropyLoss.py
@@ -0,0 +1,14 @@
+import numpy as np
+import torch
+import torch.nn as nn
+
+from dao.register import Registers
+
+
+@Registers.losses.register
+def CrossEntropyLoss(weight=None, ignore_index=255, reduction='mean'):
+        weight = torch.from_numpy(np.array(weight)).float()
+        CE = nn.CrossEntropyLoss(weight=weight, ignore_index=ignore_index, reduction=reduction)
+        return CE
+
+
