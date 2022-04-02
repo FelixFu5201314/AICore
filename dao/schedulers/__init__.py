1fe23ec6fa6428a30d40415f7ddbabe65358919b
f4396 <fafu@lusterinc.com>
2022-04-02 17:00:41 +0800

    Yolox schedulers

--- dao/schedulers/__init__.py
+++ dao/schedulers/__init__.py
@@ -8,3 +8,5 @@ from .multistep_lr import multistep_lr
 from .cos_lr import cos_lr
 from .poly import poly
 
+from .yolox_warm_cos import yolox_warm_cos
+from .yolox_semi_warm_cos_lr import yolox_semi_warm_cos_lr
