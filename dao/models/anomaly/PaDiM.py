de5fe832f4b9058b3f507bc07bdaa3260aab1d8c
f4396 <fafu@lusterinc.com>
2022-04-12 16:40:14 +0800

    修改成和PaDim官方代码一致

--- dao/models/anomaly/PaDiM.py
+++ dao/models/anomaly/PaDiM.py
@@ -13,7 +13,7 @@ from collections import OrderedDict
 
 import numpy as np
 from loguru import logger
-
+from random import sample
 from sklearn.metrics import roc_auc_score
 from sklearn.metrics import roc_curve
 from sklearn.metrics import precision_recall_curve
@@ -25,6 +25,9 @@ from skimage import morphology
 from skimage.segmentation import mark_boundaries
 
 import torch
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+from torchvision.models import wide_resnet50_2, resnet18
 import timm
 import torch.nn.functional as F
 
@@ -33,65 +36,78 @@ from dao.register import Registers
 
 @Registers.anomaly_models.register
 class PaDiM:
-    def __init__(self,
-                 backbone,
-                 device=None,
-                 d_reduced: int = 100,
-                 image_size=224,
-                 beta=1,
-                 ):
-        self.feature_extractor = timm.create_model(
-            backbone.type,
-            **backbone.kwargs
-        )
-        for param in self.feature_extractor.parameters():
-            param.requires_grad = False
-        self.feature_extractor.eval()
-        self.feature_extractor = self.feature_extractor.to(device)
+    def __init__(self, backbone, device=None, d_reduced: int = 100, total_dim=None, image_size=224, beta=1):
+        # backbone load model
+        if backbone.type == 'resnet18':
+            self.model = resnet18(pretrained=True, progress=True)
+        elif backbone.type == 'wide_resnet50_2':
+            self.model = wide_resnet50_2(pretrained=True, progress=True)
+        self.model.to(device)
+        self.model.eval()
+        # set model's intermediate outputs
+        self.outputs = []
+        def hook(module, input, output):
+            self.outputs.append(output)
+        self.model.layer1[-1].register_forward_hook(hook)
+        self.model.layer2[-1].register_forward_hook(hook)
+        self.model.layer3[-1].register_forward_hook(hook)
 
         self.image_size = image_size
-        self.d_reduced = d_reduced  # your RAM will thank you
+        self.d_reduced = d_reduced  # your RAM will thank you   选取维度
+        self.t_d = total_dim    # 总维度特征
         self.beta = beta
         self.device = device
 
-        # random.seed(1024)
-        # torch.manual_seed(1024)
-        # if self.device is not None:
-        #     torch.cuda.manual_seed_all(1024)
-
-    def fit(self, train_dl, output_dir=None):
-        # extract train set features
-        train_feature_filepath = os.path.join(output_dir, 'features.pkl')
-        if not os.path.exists(train_feature_filepath):
-            feature_maps = []
-            for image, mask, label, image_path in tqdm(train_dl, desc="Train: "):
+    def fit(self, train_dataloader, output_dir=None):
+        # extract train set features 提取特征
+        train_feature_filepath = os.path.join(output_dir, 'features.pkl')  # 特征存放路径
+        train_outputs = OrderedDict([('layer1', []), ('layer2', []), ('layer3', [])])
+        if not os.path.exists(train_feature_filepath):  # 如果特征不存在
+            # 提取特征
+            logger.info("1.1 extract train set features")
+            for i, (image, mask, label, image_path) in enumerate(train_dataloader):
+                logger.info("extract feature iter {}/{}".format(i, len(train_dataloader)))
                 # model prediction
-                feature_maps.append([fmap.to("cpu") for fmap in self.feature_extractor(image.to(self.device))])
+                with torch.no_grad():
+                    _ = self.model(image.to(self.device))
+                # get intermediate layer outputs
+                for k, v in zip(train_outputs.keys(), self.outputs):
+                    train_outputs[k].append(v.cpu().detach())
+                # initialize hook outputs
+                self.outputs = []
+            for k, v in train_outputs.items():
+                train_outputs[k] = torch.cat(v, 0)
 
             # 将feature_maps 转为 embedding_vectors: torch.Size([200, 1792, 56, 56])
-            embedding_vectors = []
-            for feature_map in feature_maps:
-                embedding_vector = feature_map[0]
-                for layer in feature_map[1:]:
-                    embedding_vector = embedding_concat(embedding_vector, layer)
-                embedding_vectors.append(embedding_vector)
-            embedding_vectors = torch.cat(embedding_vectors, dim=0)
+            logger.info("1.2 covert feature_maps to embedding_vectors ......")
+            embedding_vectors = train_outputs['layer1']
+            for layer_name in ['layer2', 'layer3']:
+                embedding_vectors = embedding_concat(embedding_vectors, train_outputs[layer_name])
+            logger.info("merge embedding_vectors, and final size {}".format(embedding_vectors.shape))
 
             # randomly select d dimension
+            logger.info("1.3 randomly select {} dimension".format(self.d_reduced))
             idx = torch.tensor(sample(range(0, embedding_vectors.shape[1]), self.d_reduced))
             embedding_vectors = torch.index_select(embedding_vectors, 1, idx)
+            logger.info("embedding_vectors:{}".format(embedding_vectors.shape))
 
             # calculate multivariate Gaussian distribution
+            logger.info("1.4 calculate multivariate Gaussian distribution")
             B, C, H, W = embedding_vectors.size()
             embedding_vectors = embedding_vectors.view(B, C, H * W)
+            logger.info("embedding_vectors view:{}".format(embedding_vectors.shape))
             mean = torch.mean(embedding_vectors, dim=0).numpy()
+            logger.info("cal mean:{}".format(mean.shape))
             cov = torch.zeros(C, C, H * W).numpy()
             I = np.identity(C)
-
-            for i in tqdm(range(H * W), desc="Train calculate cov:"):
+            logger.info("cal cov .......")
+            for i in range(H * W):
                 cov[:, :, i] = np.cov(embedding_vectors[:, :, i].numpy(), rowvar=False) + 0.01 * I
+            logger.info("cov:{}".format(cov.shape))
+
             # save learned distribution
-            train_outputs = [mean, cov, idx]
+            logger.info("1.5 save learned distribution")
+            train_outputs = [mean, cov, np.asarray(idx)]
             with open(train_feature_filepath, 'wb') as f:
                 pickle.dump(train_outputs, f)
         else:
@@ -101,40 +117,51 @@ class PaDiM:
 
         self.train_output = train_outputs
 
-    def evaluate(self, test_dl, output_dir=None):
+    def evaluate(self, test_dataloader, output_dir=None):
         gt_list = []
         gt_mask_list = []
         test_imgs = []
         test_imgs_path = []
         self.test_outputs = []
+        test_outputs = OrderedDict([('layer1', []), ('layer2', []), ('layer3', [])])
 
-        feature_maps = []
         # extract test set features
-        for image, mask, label, image_path in tqdm(test_dl, desc="Evaluate: "):
-            test_imgs.extend(image.cpu().detach().numpy())
-            gt_list.extend(label.cpu().detach().numpy())
-            gt_mask_list.extend(mask.cpu().detach().numpy())
-            test_imgs_path.extend(image_path)
-
-            feature_maps.append([fmap.to("cpu") for fmap in self.feature_extractor(image.to(self.device))])
+        logger.info("2.1 extract test set features")
+        for i, (image, y, mask, image_path) in enumerate(test_dataloader):
+            test_imgs.extend(image.cpu().detach().numpy())  # 所有图片
+            gt_list.extend(y.cpu().detach().numpy())    # 是否是good
+            gt_mask_list.extend(mask.cpu().detach().numpy())    # 所有mask
+            test_imgs_path.extend(image_path)   # 图片路径
+            # model prediction
+            with torch.no_grad():
+                _ = self.model(image.to(self.device))
+            # get intermediate layer outputs
+            for k, v in zip(test_outputs.keys(), self.outputs):
+                test_outputs[k].append(v.cpu().detach())
+            # initialize hook outputs
+            self.outputs = []
+        for k, v in test_outputs.items():
+            test_outputs[k] = torch.cat(v, 0)
 
         # 将feature_maps 转为 embedding_vectors: torch.Size([200, 1792, 56, 56])
-        embedding_vectors = []
-        for feature_map in feature_maps:
-            embedding_vector = feature_map[0]
-            for layer in feature_map[1:]:
-                embedding_vector = embedding_concat(embedding_vector, layer)
-            embedding_vectors.append(embedding_vector)
-        embedding_vectors = torch.cat(embedding_vectors, dim=0)
+        logger.info("2.2 covert feature_maps to embedding_vectors ......")
+        embedding_vectors = test_outputs['layer1']
+        for layer_name in ['layer2', 'layer3']:
+            embedding_vectors = embedding_concat(embedding_vectors, test_outputs[layer_name])
+
+        logger.info("merge embedding_vectors, and final size {}".format(embedding_vectors.shape))
 
         # randomly select d dimension
-        embedding_vectors = torch.index_select(embedding_vectors, 1, self.train_output[2])
+        logger.info("2.3 randomly select {} dimension".format(self.d_reduced))
+        embedding_vectors = torch.index_select(embedding_vectors, 1, torch.from_numpy(self.train_output[2]))
 
         # calculate distance matrix
+        logger.info("2.4 calculate multivariate Gaussian distribution, this will take a minute ......")
+        logger.info("this operate will use cpu, please Reserve sufficient resources ......")
         B, C, H, W = embedding_vectors.size()
         embedding_vectors =embedding_vectors.view(B, C, H * W).numpy()
         dist_list = []
-        for i in tqdm(range(H * W), desc="Evaluate calculate cov::"):
+        for i in range(H * W):
             mean = self.train_output[0][:, i]
             conv_inv = np.linalg.inv(self.train_output[1][:, :, i])
             dist = [mahalanobis(sample[:, i], mean, conv_inv) for sample in embedding_vectors]
@@ -143,32 +170,39 @@ class PaDiM:
         dist_list = np.array(dist_list).transpose(1, 0).reshape(B, H, W)
 
         # upsample
-        dist_list = torch.tensor(dist_list) # torch.Size([49, 56, 56])
+        logger.info("2.5 upsample")
+        dist_list = torch.tensor(dist_list)  # torch.Size([B, 56, 56])
         score_map = F.interpolate(dist_list.unsqueeze(1), size=self.image_size, mode='bilinear',
-                                  align_corners=False).squeeze().numpy()    # (49, 224, 224)
+                                  align_corners=False).squeeze().numpy()    # (B, 224, 224)
 
         # apply gaussian smoothing on the score map
+        logger.info("2.6 apply gaussian smoothing on the score map")
         for i in range(score_map.shape[0]):
             score_map[i] = gaussian_filter(score_map[i], sigma=4)
 
         # Normalization
+        logger.info("2.6 Normalization")
         max_score = score_map.max()
         min_score = score_map.min()
-        scores = (score_map - min_score) / (max_score - min_score)  # (49, 224, 224)
+        scores = (score_map - min_score) / (max_score - min_score)  # (B, 224, 224) scores是均值化后的结果
 
-        fig, ax = plt.subplots(1, 2, figsize=(20, 10))
+        # 以下是显示结果内容
+        logger.info("2.7 result .......")
+        fig, ax = plt.subplots(1, 2, figsize=(20, 10))  # 绘制ROC曲线
         fig_img_rocauc = ax[0]
         fig_pixel_rocauc = ax[1]
 
         # calculate image-level ROC AUC score
-        img_scores = scores.reshape(scores.shape[0], -1).max(axis=1)    # shape 49
-        gt_list = np.asarray(gt_list)   # shape 49
+        logger.info("calculate image-level ROC AUC score")
+        img_scores = scores.reshape(scores.shape[0], -1).max(axis=1)    # shape B
+        gt_list = np.asarray(gt_list)   # shape B
         fpr, tpr, _ = roc_curve(gt_list, img_scores)
         img_roc_auc = roc_auc_score(gt_list, img_scores)
         logger.info('image ROCAUC: %.3f' % (img_roc_auc))
         fig_img_rocauc.plot(fpr, tpr, label='img_ROCAUC: %.3f' % (img_roc_auc))
 
         # calculate per-pixel level ROCAUC
+        logger.info("calculate per-pixel level ROCAUC")
         gt_mask = np.where(np.asarray(gt_mask_list) != 0, 1, 0)  # (49, 1, 224, 224)
         fpr, tpr, _ = roc_curve(gt_mask.flatten(), scores.flatten())
         per_pixel_rocauc = roc_auc_score(gt_mask.flatten(), scores.flatten())
@@ -189,28 +223,34 @@ class PaDiM:
         threshold = thresholds[np.argmax(f1)]
 
         # 绘制每张test图片预测信息
-        # test_imgs:[(3, 224, 224), ..., batchsize]
-        # scores: (batchsize, 224, 224)
-        # gt_mask_list: [(1, 224, 224), ..., batchsize]
-        # threshold: float
-        # save_dir: str
-        # test_imgs_path: [img_path, ..., batchsize]
-        plot_fig(test_imgs, scores, gt_mask_list, threshold, save_dir, test_imgs_path)
+        mean = test_dataloader.dataset.mean
+        std = test_dataloader.dataset.std
+        plot_fig(test_imgs, scores, gt_mask_list, threshold, save_dir, test_imgs_path, mean, std)
 
-        train_feature_filepath = os.path.join(output_dir, 'features.pkl')
-        train_outputs = [self.train_output[0], self.train_output[1], self.train_output[2], threshold]
-        with open(train_feature_filepath, 'wb') as f:
-            pickle.dump(train_outputs, f)
+        threshold_txt = os.path.join(output_dir, 'threshold.txt')
+        with open(threshold_txt, 'w') as f:
+            f.write(str(threshold))
 
 
-def plot_fig(test_img, scores, gts, threshold, save_dir, class_name):
+def plot_fig(test_img, scores, gts, threshold, save_dir, test_imgs_path, mean, std):
+    """
+    将test_img,scores,gts根据threshold绘制成图像，并保存到save_dir中
+    :param test_img: test_imgs:[(3, 224, 224), ..., batchsize]
+    :param scores:  scores: (batchsize, 224, 224)
+    :param gts: gt_mask_list: [(1, 224, 224), ..., batchsize]
+    :param threshold: float
+    :param save_dir: str
+    :param class_name: [img_path, ..., batchsize]
+    :return:
+    """
     num = len(scores)
+    logger.info("number:{}".format(num))
     vmax = scores.max() * 255.
     vmin = scores.min() * 255.
     for i in range(num):
         img = test_img[i]
-        img = denormalization(img)
-        gt = gts[i].squeeze()  # .transpose(1, 2, 0)
+        img = denormalization(img, mean=mean, std=std)
+        gt = gts[i].transpose(1, 2, 0).squeeze()  # .transpose(1, 2, 0)
         heat_map = scores[i] * 255
         mask = scores[i]
         mask[mask > threshold] = 1
@@ -253,29 +293,37 @@ def plot_fig(test_img, scores, gts, threshold, save_dir, class_name):
         }
         cb.set_label('Anomaly Score', fontdict=font)
 
-        img_name = class_name[i].split("/")[-1]
-        fig_img.savefig(os.path.join(save_dir, img_name), dpi=100)
+        img_name = test_imgs_path[i].split("/")[-1][:-4]+".png"
+        ngtype = test_imgs_path[i].split("/")[-2]
+        fig_img.savefig(os.path.join(save_dir, "{}_".format(ngtype)+img_name), dpi=100)
         plt.close()
 
 
-def denormalization(x):
-    mean = np.array([0.485, 0.456, 0.406])
-    std = np.array([0.229, 0.224, 0.225])
+def denormalization(x, mean=[0.335782, 0.335782, 0.335782], std=[0.256730, 0.256730, 0.256730]):
+    mean = np.array(mean)
+    std = np.array(std)
     x = (((x.transpose(1, 2, 0) * std) + mean) * 255.).astype(np.uint8)
 
     return x
 
 
 def embedding_concat(x, y):
-    B, C1, H1, W1 = x.size()  # (209,256,56,56)
-    _, C2, H2, W2 = y.size()  # (209,512,28,28)
-    s = int(H1 / H2)
-    x = F.unfold(x, kernel_size=s, dilation=1, stride=s)  # (209, 1024, 784)
-    x = x.view(B, C1, -1, H2, W2)  # torch.Size([209, 256, 4, 28, 28])
-    z = torch.zeros(B, C1 + C2, x.size(2), H2, W2)  # torch.Size([209, 768, 4, 28, 28])
+    """
+    将x,y嵌入连接，要求x,y同batchsize大小，通道数、长宽各不相同； 拼接成x形状即[B_x, C_(x+y), H_x, W_x)
+
+    :param x:
+    :param y:
+    :return:
+    """
+    B, C1, H1, W1 = x.size()  # torch.Size([32, 256, 56, 56])
+    _, C2, H2, W2 = y.size()  # torch.Size([32, 512, 28, 28])
+    s = int(H1 / H2)    # x对于y的缩小步长
+    x = F.unfold(x, kernel_size=s, dilation=1, stride=s)  # https://blog.csdn.net/qq_34914551/article/details/102940368 torch.Size([32, 256, 56, 56])-》torch.Size([32, 1024, 784])
+    x = x.view(B, C1, -1, H2, W2)  # torch.Size([32, 256, 4, 28, 28])
+    z = torch.zeros(B, C1 + C2, x.size(2), H2, W2)  # torch.Size([32, 768, 4, 28, 28])
     for i in range(x.size(2)):
-        z[:, :, i, :, :] = torch.cat((x[:, :, i, :, :], y), 1)
-    z = z.view(B, -1, H2 * W2)  # torch.Size([209, 3072, 784])
-    z = F.fold(z, kernel_size=s, output_size=(H1, W1), stride=s)  # torch.Size([209, 768, 56, 56])
+        z[:, :, i, :, :] = torch.cat((x[:, :, i, :, :], y), 1)  # torch.cat((x[:, :, i, :, :], y), 1)=torch.Size([32, 768, 28, 28]), 其中x[:, :, i, :, :]torch.Size([32, 256, 28, 28])， ytorch.Size([32, 512, 28, 28])
+    z = z.view(B, -1, H2 * W2)  # torch.Size([32, 3072, 784])
+    z = F.fold(z, kernel_size=s, output_size=(H1, W1), stride=s)  # torch.Size([32, 768, 56, 56])
 
     return z
