68fb447cd11e9ed26f50d46320ac6ba596a6d7d6
f4396 <fafu@lusterinc.com>
2022-04-14 13:13:46 +0800

    trainer 分割

--- dao/trainers/__init__.py
+++ dao/trainers/__init__.py
@@ -3,7 +3,12 @@
 # @Date: 2021.4.14
 # @GitHub:https://github.com/felixfu520
 # @Copy From:
+
+# 所有model在__init__.py中导入，是为了自动注册到Registers中
+
 # 分类
 from .trainerCls import ClsTrainer, ClsEval, ClsDemo, ClsExport
 # 异常检测
 from .trainerAnomaly import AnomalyTrainer, AnomalyEval, AnomalyDemo, AnomalyExport
+# 分割
+from .trainerSeg import SegTrainer, SegEval, SegExport, SegDemo
