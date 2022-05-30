98d0a88b9224b9b0c1c3b882e905db1fa9b5b643
f4396 <fafu@lusterinc.com>
2022-05-30 08:23:31 +0800

    目标检测dataset注释

--- dao/dataloaders/datasets/Det2Dataset.py
+++ dao/dataloaders/datasets/Det2Dataset.py
@@ -29,16 +29,16 @@ class Det2Dataset(Dataset):
         """
         目标检测数据集
 
-        data_dir:str  数据集文件夹路径，文件夹要求是
+        data_dir:str  数据集文件夹路径，文件夹要求如下
             |-dataset
-                |- images
+                |- images   存放所有图片的文件夹
                     |-图片
-                |- labels
+                |- labels   存放所有标注文件的图片
                     |-txt文件
-                |- train.txt
-                |- val.txt
-                |- test.txt
-                |- labels.txt
+                |- train.txt    训练集相对路径
+                |- val.txt      验证集相对路径
+                |- test.txt     测试集相对路径
+                |- labels.txt   标签
 
         image_set:str "train.txt or val.txt or test.txt"
         in_channels:int  输入图片的通道数，目前只支持1和3通道
@@ -49,26 +49,25 @@ class Det2Dataset(Dataset):
         mask_suffix:str 可接受的图片后缀
         """
         # set attr
-        self.root = data_dir
-        self.preproc = preproc
-        self.image_set = image_set
-        self.in_channels = in_channels
-        self.img_size = input_size
-        self.image_suffix = image_suffix
-        self.mask_suffix = mask_suffix
+        self.root = data_dir    # 数据集路径
+        self.preproc = preproc  # albumentations预处理
+        self.image_set = image_set  # 训练集、验证集、测试集
+        self.in_channels = in_channels  # 图片通道数
+        self.img_size = input_size  # 图片宽高
+        self.image_suffix = image_suffix    # 图片文件后缀名
+        self.mask_suffix = mask_suffix  # 标注文件后缀名
 
         # 存储数据
         self.ids = []   # 存放图片路径 (image path, mask path)
         self.labels_dict = dict()  # id:name形式
 
-        self._set_ids()  # 获取所有文件名，存放到self.ids中 [(image_path, mask_path), ... ]
-        self.imgs = None
-        self.labels = None
+        self._set_ids()  # 获取所有文件名，存放到self.ids中 [(image_path, label_path), ... ]
 
     def __getitem__(self, index):
         image, bboxes, class_labels, image_path = self.pull_item(index)  # image:ndarray, label:[(class_id,x,y,h,w),...], image_path:[string jpg,string label]
-        class_labels_name = [self.labels_dict[str(tmp)] for tmp in class_labels]
 
+        # 使用albumentations增强图片
+        class_labels_name = [self.labels_dict[str(tmp)] for tmp in class_labels]    # 通过class_id获得class_name
         if self.preproc is not None:
             transformed = self.preproc(image=image, bboxes=bboxes, class_labels=class_labels_name)
             transformed_image = transformed['image']
@@ -79,6 +78,11 @@ class Det2Dataset(Dataset):
         return transformed_image, transformed_bboxes, transformed_class_labels, image_path
 
     def pull_item(self, index):
+        """
+        通过index，返回img，bboxes，图片路径
+        :param index:
+        :return:
+        """
         img, bbox, label = self._load_img(index)  # 图片（ndarray）; bbox（[(x,y,w,h)归一化后的,]); label[类别ID,...]
         if self.in_channels == 1:
             img = np.expand_dims(img.copy(), axis=2)
@@ -91,7 +95,7 @@ class Det2Dataset(Dataset):
         功能：通过index,找到文件名，
              然后获得
                     图片（ndarray）
-                    bbox（[(x,y,w,h)归一化后的,])
+                    bbox（[(x,y,w,h)归一化后的(yolo格式）,])
                     label[类别ID,...]
         :param index:
         :return:
@@ -121,12 +125,14 @@ class Det2Dataset(Dataset):
         """
         list_path = os.path.join(self.root, self.image_set)
 
+        # 获得图片路径，设置self.ids
         with open(list_path, 'r', encoding='utf-8') as images_labels:
             for line in images_labels.readlines():
                 image_path = os.path.join(self.root, "images", line.strip() + self.image_suffix).replace("\\",'/')
                 label_path = os.path.join(self.root, "labels", line.strip() + self.mask_suffix).replace("\\",'/')
                 self.ids.append((image_path, label_path))
 
+        # 获得id:name，设置self.labels_dict
         with open(os.path.join(self.root, "labels.txt"), 'r', encoding='utf-8') as labels:
             for label in labels:
                 self.labels_dict[label.split()[0]] = label.split()[1]
@@ -164,7 +170,7 @@ if __name__ == "__main__":
         "transforms": {
             "kwargs": {
                 "Resize": {"height": 416, "width": 416, "p": 1},
-                "RandomCrop": {"width": 224, "height": 224, "p": 1},
+                # "RandomCrop": {"width": 224, "height": 224, "p": 1},
                 "Flip":{"p":1},
                 "Normalize": {"mean": [0.471, 0.448, 0.408], "std": [0.234, 0.239, 0.242], "p": 1}
 
@@ -180,17 +186,12 @@ if __name__ == "__main__":
     image = denormalization(transformed_image, norm_mean=[0.471, 0.448, 0.408], norm_std=[0.234, 0.239, 0.242])
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     height, width = image.shape[0], image.shape[1]
-    # transformed_bboxes = cxcywh2xyxy(transformed_bboxes)
     for bbox, labelName in zip(transformed_bboxes, transformed_class_labels):
-        # x1 = int(bbox[0] * width)
-        # y1 = int(bbox[1] * height)
-        # x2 = int(bbox[2] * width)
-        # y2 = int(bbox[3] * height)
+        # cxcywh2xyxy
         center_x = round(bbox[0] * width)
         center_y = round(bbox[1] * height)
         bbox_width = round(bbox[2] * width)
         bbox_height = round(bbox[3] * height)
-
         xmin = int(center_x - bbox_width / 2)
         ymin = int(center_y - bbox_height / 2)
         xmax = int(center_x + bbox_width / 2)
