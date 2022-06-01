076010c0e6bca96c969c26ba2b64c0a59f5d5b6c
f4396 <fafu@lusterinc.com>
2022-06-01 17:32:16 +0800

    dataloader modules 220601

--- dao/dataloaders/__init__.py
+++ dao/dataloaders/__init__.py
@@ -6,14 +6,18 @@
 
 # 所有model在__init__.py中导入，是为了自动注册到Registers中
 
-# 分类
+# 分类: Classification
 from .datasets import ClsDataset
 from .ClsDataloader import ClsDataloaderTrain, ClsDataloaderEval
 
-# 异常检测
+# 异常检测: AnomalyDetection
 from .datasets import MVTecDataset
 from .AnomalyDataloader import MVTecDataloader
 
-# 分割
+# 分割: Segmentation
 from .datasets import SegDataset
 from .SegDataloader import SegDataloaderTrain, SegDataloaderEval
+
+# 目标检测: ObjectDetection
+from .datasets import DetDataset
+from .DetDataloader import DetDataloaderTrain, DetDataloaderEval
