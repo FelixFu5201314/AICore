a364e8aeaeac856f8d613cbe555a35c6f4714b28
f4396 <fafu@lusterinc.com>
2022-06-02 13:17:44 +0800

    obejctdetection return bboxes,labels合并 220602

--- dao/dataloaders/DetDataloader.py
+++ dao/dataloaders/DetDataloader.py
@@ -6,11 +6,12 @@
 
 from loguru import logger
 
+import torch
 import torch.multiprocessing
 
 from dao.dataloaders.augments import get_transformer, get_transformerYOLO
 from dao.dataloaders.augments import TrainTransform, ValTransform
-from dao.dataloaders.dataloading import DataLoader, worker_init_reset_seed
+from dao.dataloaders.dataloading import DataLoader, worker_init_reset_seed, detection_collate
 from dao.dataloaders.samplers import InfiniteSampler, YoloBatchSampler, BatchSampler
 from dao.utils import wait_for_the_master, get_local_rank, get_world_size
 
@@ -57,10 +58,21 @@ def DetDataloaderTrain(is_distributed=False, batch_size=None, num_workers=None, 
     # Make sure each process has different random seed, especially for 'fork' method.
     # Check https://github.com/pytorch/pytorch/issues/63311 for more details.
     dataloader_kwargs["worker_init_fn"] = worker_init_reset_seed
+    dataloader_kwargs["collate_fn"] = detection_collate
 
     # 7. 生成Dataloader类， 具体看dataloading文件内容
     train_loader = DataLoader(dataset_Det, **dataloader_kwargs)
     return train_loader
+
+
+    # dataloader = torch.utils.data.DataLoader(
+    #     dataset=dataset,
+    #     batch_size=args.batch_size,
+    #     collate_fn=detection_collate,
+    #     num_workers=args.num_workers,
+    #     pin_memory=True,
+    #     sampler=torch.utils.data.distributed.DistributedSampler(dataset)
+    # )
 
 
 @Registers.dataloaders.register
@@ -98,3 +110,44 @@ def DetDataloaderEval(is_distributed=False, batch_size=None, num_workers=None, d
     val_loader = torch.utils.data.DataLoader(valdataset, **dataloader_kwargs)
     return val_loader
 
+
+if __name__ == "__main__":
+    from dao.dataloaders.augments import get_transformerYOLO, get_transformer
+    from dotmap import DotMap
+    dataloader_c = {
+        "type": "DetDataloaderTrain",
+        "dataset": {
+            "type": "DetDataset",
+            "kwargs": {
+                "data_dir": "/ai/data/AIDatasets/ObjectDetection/4AR6N-L546S-DQSM9-424ZM-N4DZ2/voc0712",
+                "image_set": "train.txt",
+                "in_channels": 3,
+                "image_suffix": ".jpg",
+                "mask_suffix": ".txt"
+            },
+            "transforms": {
+                "kwargs": {
+                    "Resize": {"height": 416, "width": 416, "p": 1},
+                    "Normalize": {
+                        "mean": [0.45289162, 0.43158466, 0.3984241],
+                        "std": [0.2709828, 0.2679657, 0.28093508], "p": 1
+                    }
+                }
+            }
+        },
+        "kwargs": {
+            "num_workers": 8,
+            "batch_size": 16
+        }
+    }
+    dataloader_c = DotMap(dataloader_c)
+    det_d = DetDataloaderTrain(
+        is_distributed=get_world_size() > 1,
+        dataset=dataloader_c.dataset,
+        seed=0,
+        ** dataloader_c.kwargs
+    )
+    for images, labels, paths in det_d:
+        print(type(images))
+        print(type(labels))
+        print(type(paths))
