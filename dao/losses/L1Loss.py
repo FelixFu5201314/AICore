dc01e8af762314e0c755edc92357ac40aac10b0c
f4396 <fafu@lusterinc.com>
2022-06-07 13:27:10 +0800

    L1Loss

---
+++ dao/losses/L1Loss.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# @Author:FelixFu
+# @Date: 2021.4.14
+# @GitHub:https://github.com/felixfu520
+# @Ref:https://pytorch.org/docs/stable/generated/torch.nn.L1Loss.html#torch.nn.L1Loss
+
+import numpy as np
+
+import torch
+
+from dao.register import Registers
+
+
+@Registers.losses.register
+def L1Loss_DAO(reduction="none"):
+    return torch.nn.L1Loss(reduction=reduction)
+
+
+if __name__ == "__main__":
+    loss = L1Loss_DAO()
+    input = torch.randn(3, 5, requires_grad=True)
+    target = torch.randn(3, 5)
+    output = loss(input, target)
+    output.backward()
