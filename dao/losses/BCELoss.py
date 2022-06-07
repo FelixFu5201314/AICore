99bca2ee86154a531a57a5f329ef84297938039b
f4396 <fafu@lusterinc.com>
2022-06-07 15:19:42 +0800

    BCELoss

---
+++ dao/losses/BCELoss.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# @Author:FelixFu
+# @Date: 2021.4.14
+# @GitHub:https://github.com/felixfu520
+# @Ref:https://pytorch.org/docs/stable/generated/torch.nn.BCELoss.html#torch.nn.BCELoss
+import numpy as np
+import torch
+import torch.nn as nn
+
+from dao.register import Registers
+
+
+@Registers.losses.register
+def BCELoss_DAO(weight=None, reduction='mean'):
+    if weight is not None:
+        weight = torch.from_numpy(np.array(weight))
+    return nn.BCELoss(weight=weight,  reduction=reduction)
+
+
+if __name__ == "__main__":
+    m = nn.Sigmoid()
+    loss = BCELoss_DAO()
+    input = torch.randn(3, requires_grad=True)
+    target = torch.empty(3).random_(2)
+    output = loss(m(input), target)
+    output.backward()
