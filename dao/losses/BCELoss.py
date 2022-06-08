f85f56d94bea218c405b4a7a1b787c2b932baf6b
f4396 <fafu@lusterinc.com>
2022-06-08 10:56:53 +0800

    Binary Cross-Entropy：二进制交叉熵损失函数

--- dao/losses/BCELoss.py
+++ dao/losses/BCELoss.py
@@ -18,10 +18,58 @@ def BCELoss(weight=None, reduction='mean'):
 
 
 if __name__ == "__main__":
+    N, C, H, W = 2, 3, 4, 4
     m = nn.Sigmoid()
-    loss = BCELoss()
-    input = torch.randn(3, requires_grad=True)
-    target = torch.empty(3).random_(2)
+    loss = BCELoss(reduction="none")
+    # 1. 测试分类
+    input = torch.randn((N, C), requires_grad=True)
+    target = torch.empty((N, C)).random_(C)
     output = loss(m(input), target)
-    print(output)
-    output.backward()
+    print("input:{}".format(m(input)))
+    print("target:{}".format(target))
+    print("output:{}".format(output))
+    torch.mean(output).backward()
+
+    # 2. 测试分割
+    input = torch.randn((N, C, H, W), requires_grad=True)
+    target = torch.empty((N, C, H, W)).random_(C)
+    output = loss(m(input), target)
+    print("input:{}".format(m(input)))
+    print("target:{}".format(target))
+    print("output:{}".format(output))
+    torch.mean(output).backward()
+
+    # 3. 测试分割(自定义数据)
+    input = [[
+        [[0, 1, 1, 0],
+         [1, 0, 0, 1],
+         [1, 0, 0, 1],
+         [0, 1, 1, 0]],
+        [[0, 0, 0, 0],
+         [0, 0, 0, 0],
+         [0, 0, 0, 0],
+         [0, 0, 0, 0]],
+        [[1, 0, 0, 1],
+         [0, 1, 1, 0],
+         [0, 1, 1, 0],
+         [1, 0, 0, 1]]]]
+    target = [[
+            [[0, 1, 1, 0],
+             [1, 0, 0, 1],
+             [1, 0, 0, 1],
+             [0, 1, 1, 0]],
+            [[0, 0, 0, 0],
+             [0, 0, 0, 0],
+             [0, 0, 0, 0],
+             [0, 0, 0, 0]],
+            [[1, 0, 0, 1],
+             [0, 1, 1, 0],
+             [0, 1, 1, 0],
+             [1, 0, 0, 1]]]]
+    input = torch.tensor(input, dtype=torch.float32, requires_grad=True)
+    target = torch.tensor(target, dtype=torch.float32)
+    output = loss(input, target)
+    print("input:{}".format(m(input)))
+    print("target:{}".format(target))
+    print("output:{}".format(output))
+    torch.mean(output).backward()
