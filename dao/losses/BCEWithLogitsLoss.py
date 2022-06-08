b5bdb7aae248543dcec0b8f822f20a19cedfa6bc
f4396 <fafu@lusterinc.com>
2022-06-08 11:36:14 +0800

    BCEWithLogitsLoss

--- dao/losses/BCEWithLogitsLoss.py
+++ dao/losses/BCEWithLogitsLoss.py
@@ -11,13 +11,83 @@ from dao.register import Registers
 
 
 @Registers.losses.register
-def BCEWithLogitsLoss(pos_weight=None, reduction='mean'):
-    return nn.BCEWithLogitsLoss(weight=pos_weight,  reduction=reduction)
+def BCEWithLogitsLoss(weight=None, reduction='mean', pos_weight=None):
+    return nn.BCEWithLogitsLoss(weight=weight,  reduction=reduction, pos_weight=pos_weight)
 
 
 if __name__ == "__main__":
-    target = torch.ones([10, 64], dtype=torch.float32)  # 64 classes, batch size = 10
-    output = torch.full([10, 64], 1.5)  # A prediction (logit)
-    pos_weight = torch.ones([64])  # All weights are equal to 1
-    criterion = BCEWithLogitsLoss(pos_weight=pos_weight)
-    criterion(output, target)  # -log(sigmoid(1.5))
+    N, C, H, W = 2, 3, 4, 4
+    loss = BCEWithLogitsLoss(reduction="none")
+    # 1. 测试分类
+    input = torch.randn((N, C), requires_grad=True)
+    target = torch.empty((N, C)).random_(C)
+    output = loss(input, target)
+    print("input:{}".format(input))
+    print("target:{}".format(target))
+    print("output:{}".format(output))
+    torch.mean(output).backward()
+
+    # 2. 测试分割
+    input = torch.randn((N, C, H, W), requires_grad=True)
+    target = torch.empty((N, C, H, W)).random_(C)
+    output = loss(input, target)
+    print("input:{}".format(input))
+    print("target:{}".format(target))
+    print("output:{}".format(output))
+    torch.mean(output).backward()
+
+    # 3. 测试分割(自定义数据)
+    input = [[
+        [[0, 1, 1, 0],
+         [1, 0, 0, 1],
+         [1, 0, 0, 1],
+         [0, 1, 1, 0]],
+        [[0, 0, 0, 0],
+         [0, 0, 0, 0],
+         [0, 0, 0, 0],
+         [0, 0, 0, 0]],
+        [[1, 0, 0, 1],
+         [0, 1, 1, 0],
+         [0, 1, 1, 0],
+         [1, 0, 0, 1]]]]
+    target = [[
+        [[0, 1, 1, 0],
+         [1, 0, 0, 1],
+         [1, 0, 0, 1],
+         [0, 1, 1, 0]],
+        [[0, 0, 0, 0],
+         [0, 0, 0, 0],
+         [0, 0, 0, 0],
+         [0, 0, 0, 0]],
+        [[1, 0, 0, 1],
+         [0, 1, 1, 0],
+         [0, 1, 1, 0],
+         [1, 0, 0, 1]]]]
+    input = torch.tensor(input, dtype=torch.float32, requires_grad=True)
+    target = torch.tensor(target, dtype=torch.float32)
+    output = loss(input, target)
+    print("input:{}".format(input))
+    print("target:{}".format(target))
+    print("output:{}".format(output))
+    torch.mean(output).backward()
+
+    # 4. 测试分类(Weighted Binary Cross-Entropy加权交叉熵损失函数)
+    loss = BCEWithLogitsLoss(reduction="none", pos_weight=torch.empty(C).random_(C))
+    input = torch.randn((N, C), requires_grad=True)
+    target = torch.empty((N, C)).random_(C)
+    output = loss(input, target)
+    print("input:{}".format(input))
+    print("target:{}".format(target))
+    print("output:{}".format(output))
+    torch.mean(output).backward()
+
+    # 4. 测试分割(Weighted Binary Cross-Entropy加权交叉熵损失函数)
+    # TODO 报错
+    loss = BCEWithLogitsLoss(reduction="none", pos_weight=torch.ones(C))
+    input = torch.randn((N, C, H, W), requires_grad=True)
+    target = torch.empty((N, C, H, W)).random_(C)
+    output = loss(input, target)
+    print("input:{}".format(input))
+    print("target:{}".format(target))
+    print("output:{}".format(output))
+    torch.mean(output).backward()
