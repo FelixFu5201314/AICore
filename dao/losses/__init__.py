72e41e6c8228b0167ac2f4ad249ec7caf3ba1327
f4396 <fafu@lusterinc.com>
2022-06-08 18:10:27 +0800

    focal loss

--- dao/losses/__init__.py
+++ dao/losses/__init__.py
@@ -6,42 +6,75 @@
 
 # 所有model在__init__.py中导入，是为了自动注册到Registers中
 
-# 1, L1 Loss, https://www.cvmart.net/community/detail/4879
-# 也称为Mean Absolute Error，即平均绝对误差（MAE），它衡量的是预测值与真实值之间距离的平均误差幅度，作用范围为0到正无穷。
-# 优点： 收敛速度快，能够对梯度给予合适的惩罚权重，而不是“一视同仁”，使梯度更新的方向可以更加精确。
-# 缺点： 对异常值十分敏感，梯度更新的方向很容易受离群点所主导，不具备鲁棒性。
-from .L1Loss import L1Loss_DAO
+"""
+[A survey of loss functions for semantic segmentation](https://paperswithcode.com/paper/a-survey-of-loss-functions-for-semantic)
+"""
+"""
+损失函数                         概念                              优点                                 缺点
+1,NLLLoss
+2,CE
+3,BCE
+4,Focal
+5,
+"""
+# * negative log likelihood loss
+# 概念：极大似然估计：https://zhuanlan.zhihu.com/p/26614750；https://blog.csdn.net/zengxiantao1994/article/details/72787849
+# torch API: https://pytorch.org/docs/stable/generated/torch.nn.NLLLoss.html#torch.nn.NLLLoss
+from .NLLLoss import NLLLoss
 
-# 2, L2 Loss, https://www.cvmart.net/community/detail/4879
-# 也称为Mean Squred Error，即均方差（MSE），它衡量的是预测值与真实1值之间距离的平方和，作用范围同为0到正无穷。
-# 优点： 对离群点（Outliers）或者异常值更具有鲁棒性。
-# 缺点： 在0点处的导数不连续，使得求解效率低下，导致收敛速度慢；而对于较小的损失值，其梯度也同其他区间损失值的梯度一样大，所以不利于网络的学习。
-from .MSELoss import MSELoss_DAO
-
-# 3, 负对数似然损失 NLLLoss
-from .NLLLoss import NLLLoss_DAO
-
-# 4, 交叉熵损失 CrossEntropyLoss
+# ------------------- Distribution-based loss for cls,seg-----------------------
+# 1.Cross-Entropy 交叉熵损失损失, 用于多分类. Cross-Entropy = Softmax + log + NLLLoss
+# 概念: https://blog.csdn.net/tsyccnh/article/details/79163834
+# torch API(如何使用?): https://pytorch.org/docs/stable/generated/torch.nn.CrossEntropyLoss.html#torch.nn.CrossEntropyLoss
+#       在分割中，pred需要输出n_class个通道
 from .CrossEntropyLoss import CrossEntropyLoss
-from .CrossEntropyLoss import CrossEntropyLoss as CrossEntropyLoss_DAO
 
-# 5,
-from .PoissonNLLLoss import PoissonNLLLoss_DAO
+# 2.Binary Cross-Entropy 二进制交叉熵损失函数，适用于二分类和多分类
+# 概念：https://www.jianshu.com/p/5b01705368bb ； https://blog.csdn.net/zhuangyuan7838/article/details/121267301
+# 如何使用：https://pytorch.org/docs/stable/generated/torch.nn.BCELoss.html#torch.nn.BCELoss
+#       在分割中，pred需要输出一个通道；
+#       面对二分类问题时，CELoss是Softmax + BCELoss
+#       如此BCELoss相比CELoss在解决多分类问题的优势就表现了出来。CELoss只是根据每行的分类结果去取值，而BCELoss考虑了每行全部结果
+from .BCELoss import BCELoss
+from .BCEWithLogitsLoss import BCEWithLogitsLoss
 
-# 6,
-from .BCELoss import BCELoss_DAO
+# 3. Focal Loss
+# 概念：https://zhuanlan.zhihu.com/p/28527749;https://blog.csdn.net/cp1314971/article/details/105559545
+#       https://cloud.tencent.com/developer/article/1669261
+# 如何使用：
+# focal loss解决了什么问题？
+# （1）不同类别不均衡
+# （2）难易样本不均衡
+from .FocalLoss import FocalLoss, FocalLoss_det_cls
 
-# 7,
-from .BCEWithLogitsLoss import BCEWithLogitsLoss_DAO
 
-# 8,
-from .MarginRankingLoss import MarginRankingLoss_DAO
+# ------------------- Boundary-based loss ---------------------------
+# 1.BoundaryLoss
+# 概念：TODO 未找到好的解释博客
+# 如何使用？：参考代码
+from .BoundaryLoss import BoundaryLoss
 
-# 9,
-from .Dice import DiceLoss_DAO
+# ------------------- Region-based loss -----------------------------
+# 1.Dice损失
+# 概念：https://blog.csdn.net/baidu_36511315/article/details/105217674
+# 如何使用：
+from .Dice import DiceLoss
 
-# 10,
-from .EuclideanDistance import EuclideanDistance
+# 2. IOU loss
+# 概念：https://blog.csdn.net/weixin_42990464/article/details/104260043
+# 如何使用：
+from .IOULoss import IoULoss
+
+# 3.
+
+
+# ------------------- Compounded loss -------------------------------
+from .DiceBCELoss import DiceBCELoss
+
+
+
+
+
 
 
 
