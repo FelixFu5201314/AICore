37333d0913b89e6550d7966c5f709572ac1f94f7
f4396 <fafu@lusterinc.com>
2022-06-09 11:42:23 +0800

    损失函数和距离

--- dao/losses/__init__.py
+++ dao/losses/__init__.py
@@ -7,14 +7,23 @@
 # 所有model在__init__.py中导入，是为了自动注册到Registers中
 
 """
-[A survey of loss functions for semantic segmentation](https://paperswithcode.com/paper/a-survey-of-loss-functions-for-semantic)
+Pytorch 官方的一些距离,损失
 """
-
-# * negative log likelihood loss
+# 1. negative log likelihood loss
 # 概念：极大似然估计：https://zhuanlan.zhihu.com/p/26614750；https://blog.csdn.net/zengxiantao1994/article/details/72787849
 # torch API: https://pytorch.org/docs/stable/generated/torch.nn.NLLLoss.html#torch.nn.NLLLoss
 from .NLLLoss import NLLLoss
 
+# 2. L1损失
+from .L1Loss import L1Loss
+
+# 3. L2损失 均方误差
+from .MSELoss import MSELoss
+
+
+"""
+[A survey of loss functions for semantic segmentation](https://paperswithcode.com/paper/a-survey-of-loss-functions-for-semantic)
+"""
 # ------------------- Distribution-based loss for cls,seg-----------------------
 # 1.Cross-Entropy 交叉熵损失损失, 用于多分类. Cross-Entropy = Softmax + log + NLLLoss
 # 概念: https://blog.csdn.net/tsyccnh/article/details/79163834
@@ -86,9 +95,52 @@ from .DiceBCELoss import DiceBCELoss
 
 """
 列出所有距离计算
+https://www.cvmart.net/community/detail/2982
 """
+# ------------------------- 常见的距离算法 --------------------------
 # 1. 欧式距离等P-norm距离
 from .EuclideanDistance import EuclideanDistance
+
+# 2. EMA Earth Mover's Distance
+# TODO
+
+# 3. Manhattan Distance
+# TODO
+
+# 4. Jaccard Distance
+# 参见IOULoss
+
+# 5. Mahalanobis Distance
+# TODO
+
+# 6. Chebyshev Distance 切比雪夫距离
+# TODO
+
+# 7. Minkowski Distance 明可夫斯基距离
+# TODO
+
+# 8. Mahalanobis Distance 马哈拉诺比斯距离
+# TODO
+
+# ---------------------- 常见的相似度（系数）算法 ---------------------
+# 1. Cosine Similarity 余弦相似度
+from .CosineSimilarity import CosineSimilarity
+
+
+# 2. Pearson Correlation Coefficient 皮尔森相关系数
+# TODO
+
+# 3. Kullback-Leibler Divergence KL散度
+from .KLDivLoss import KLDivLoss
+
+# 4. Jaccard Coefficient Jaccard相似系数
+# 参考IOULoss
+
+# 5. Tanimoto系数（广义Jaccard相似系数）
+# TODO
+
+# 6. Mutual Information 互信息
+# TODO
 
 
 
