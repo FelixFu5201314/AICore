24ada1c8acf8247f57ebe35b1891cf475f8ebd27
f4396 <fafu@lusterinc.com>
2022-06-09 11:35:11 +0800

    KL散度

--- dao/losses/KLDivLoss.py
+++ dao/losses/KLDivLoss.py
@@ -12,16 +12,17 @@ from dao.register import Registers
 
 
 @Registers.losses.register
-def KLDivLoss_DAO(reduction="mean"):
+def KLDivLoss(reduction="mean"):
     return torch.nn.KLDivLoss(reduction=reduction)
 
 
 if __name__ == "__main__":
     from torch.nn import functional as F
-    kl_loss = KLDivLoss_DAO(reduction="batchmean")
+    kl_loss = KLDivLoss(reduction="batchmean")
     # input should be a distribution in the log space
     input = F.log_softmax(torch.randn(3, 5, requires_grad=True))
     # Sample a batch of distributions. Usually this would come from the dataset
     target = F.softmax(torch.rand(3, 5))
     output = kl_loss(input, target)
+    print(output)
 
